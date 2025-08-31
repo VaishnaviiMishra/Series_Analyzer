b010b021e5a30cf531f2b3dbe1e9fb1a74471921
VaishnaviMishra <vaishnavisudha111@gmail.com>
2025-08-31 16:18:10 +0530

    Character_chatbot

--- gradio_app.py
+++ gradio_app.py
@@ -2,35 +2,31 @@ import gradio as gr
 from theme_classifier import ThemeClassifier
 from character_network import NamedEntityRecognizer, CharacterNetworkGenerator
 from text_classification import JutsuClassifier
+from character_chatbot import CharacterChatBot
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-def get_themes(theme_list_str, subtitles_path, save_path):
+def get_themes(theme_list_str,subtitles_path,save_path):
     theme_list = theme_list_str.split(',')
     theme_classifier = ThemeClassifier(theme_list)
-    output_df = theme_classifier.get_themes(subtitles_path, save_path)
-    
-    # Remove dialogue from the theme_list
+    output_df = theme_classifier.get_themes(subtitles_path,save_path)
+    # Remove dialogue from the theme list
     theme_list = [theme for theme in theme_list if theme != 'dialogue']
     output_df = output_df[theme_list]
-    
     output_df = output_df[theme_list].sum().reset_index()
-    output_df.columns = ['Theme', 'Score']
-    
+    output_df.columns = ['Theme','Score']
     output_chart = gr.BarPlot(
         output_df,
         x="Theme",
         y="Score",
         title="Series Themes",
-        tooltip=["Theme", "Score"],
+        tooltip=["Theme","Score"],
         vertical=False,
         width=500,
         height=260
     )
-    
     return output_chart
-
 
 def get_character_network(subtitles_path,ner_path):
     ner = NamedEntityRecognizer()
@@ -45,17 +41,26 @@ def get_character_network(subtitles_path,ner_path):
 def classify_text(text_classifcation_model,text_classifcation_data_path,text_to_classify):
     jutsu_classifier = JutsuClassifier(model_path = text_classifcation_model,
                                        data_path = text_classifcation_data_path,
-                                       huggingface_token = os.getenv('huggingface_token '))
+                                       huggingface_token = os.getenv('huggingface_token'))
     
     output = jutsu_classifier.classify_jutsu(text_to_classify)
     output = output[0]
     
     return output
 
+def chat_with_character_chatbot(message, history):
+    character_chatbot = CharacterChatBot("vaishnaviiii34/Naruto_Llama-3-8B",
+                                         huggingface_token = os.getenv('huggingface_token')
+                                         )
+
+    output = character_chatbot.chat(message, history)
+    output = output['content'].strip()
+    return output
+
 
 def main():
     with gr.Blocks() as iface:
-         # Theme Classification Section
+        # Theme Classification Section
         with gr.Row():
             with gr.Column():
                 gr.HTML("<h1>Theme Classification (Zero Shot Claasifiers)</h1>")
@@ -96,6 +101,11 @@ def main():
                         classify_text_button = gr.Button("Clasify Text (Jutsu)")
                         classify_text_button.click(classify_text, inputs=[text_classifcation_model,text_classifcation_data_path,text_to_classify], outputs=[text_classification_output])
 
+        # Character Chatbot Section
+        with gr.Row():
+            with gr.Column():
+                gr.HTML("<h1>Character Chatbot</h1>")
+                gr.ChatInterface(chat_with_character_chatbot)
 
     iface.launch(share=True)
 
